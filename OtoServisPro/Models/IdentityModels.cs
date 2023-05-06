06eb96eee021cbffd6e221bce92d79cc69cb9637
nour974 <nourbaroudi95@gmail.com>
2023-05-06 23:25:51 +0300

    Data Context Sınıfı Oluşturuldu.

--- OtoServisPro/Models/IdentityModels.cs
+++ OtoServisPro/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace OtoServis.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("DataContext", throwIfV1Schema: false)
         {
         }
 
