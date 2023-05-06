a7fc2c8305a676aa67d5df0441ed81e9acb310f8
Mehmet Şükrü Kuşulay <sfakmlmnh_9@hotmail.com>
2023-05-06 21:52:57 +0300

    Business Layer (IRespository) eklendi

---
+++ OtoServisPro.BusinessLayer/Abstract/IRespository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OtoServisPro.BusinessLayer.Abstract
+{
+    public interface IRespository<T>
+    {
+        List<T> List();
+        T GetById(int id);
+        void Insert(T t);
+        void Delete(T t);
+        void Update(T t);
+        void Save();
+        IEnumerable<T> Get(Expression<Func<T,bool>> filter=null,
+            Func<IQueryable<T>,
+                IOrderedQueryable<T>> orderBy=null,
+            string includeProperti= "");
+
+
+    }
+}
