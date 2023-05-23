a8aee130d9d01c511d555763e9692325b5b9f9e7
Mehmet Şükrü Kuşulay <sfakmlmnh_9@hotmail.com>
2023-05-23 14:44:12 +0300

    hata gidermeleri

---
+++ OtoServisPro.DataAccessLayer/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace OtoServisPro.DataAccessLayer.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<OtoServisPro.DataAccessLayer.DataContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
+        }
+
+        protected override void Seed(OtoServisPro.DataAccessLayer.DataContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
