0c4be0d5fca1ca7800b1dff2a2b755e1ee9b218c
Mehmet Şükrü Kuşulay <sfakmlmnh_9@hotmail.com>
2023-05-24 00:52:23 +0300

    Kampanya/Duyuru - Admin Paneli eklendi

--- OtoServisPro.DataAccessLayer/DataContext.cs
+++ OtoServisPro.DataAccessLayer/DataContext.cs
@@ -11,5 +11,6 @@ namespace OtoServisPro.DataAccessLayer
     public class DataContext : DbContext
     {
         public DbSet<Slider> Sliders { get; set; }
+        public DbSet <Kampanya> Kampanyas { get; set; }
     }
 }
---
+++ OtoServisPro/Controllers/Web/KampanyaController.cs
@@ -0,0 +1,21 @@
+﻿using OtoServisPro.BusinessLayer.Abstract;
+using OtoServisPro.Entities.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OtoServisPro.Controllers.Web
+{
+    public class KampanyaController : Controller
+    {
+        Repository<Kampanya> rpKampanya = new Repository<Kampanya>();
+        // GET: Kampanya
+        public ActionResult Index()
+        {
+            var kampanya = rpKampanya.List().FirstOrDefault();
+            return View(kampanya);
+        }
+    }
+}
