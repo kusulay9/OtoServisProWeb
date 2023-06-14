01d2fd2a35874ee8c2ded14899f07c049f72762b
Mehmet Şükrü Kuşulay <sfakmlmnh_9@hotmail.com>
2023-06-14 23:15:37 +0300

    İş Emri Oluşturma Sayfası Eklendi

---
+++ OtoServisPro/Controllers/Servis/IsemriController.cs
@@ -0,0 +1,32 @@
+﻿using OtoServisPro.BusinessLayer.Abstract;
+using OtoServisPro.Entities.Servis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OtoServisPro.Controllers.Servis
+{
+    public class IsemriController : Controller
+    {
+        private readonly Repository<Musteri> rpMusteri = new Repository<Musteri>();
+        // GET: Isemri
+        public ActionResult Index(string ara)
+        {
+            if (ara=="" || ara==null)
+            {
+                var musteri = rpMusteri.Get().OrderByDescending(x => x.MusteriId).Take(20).ToList();
+                return View(musteri);
+            }
+            var musteriAra = rpMusteri.Get(x => x.AdSoyad.Contains(ara) || x.Telefon.Contains(ara)).ToList();
+            return View(musteriAra);
+            
+        }
+
+        public ActionResult IsemriOlustur(int musteriid)
+        {
+            return View();
+        }
+    }
+}
